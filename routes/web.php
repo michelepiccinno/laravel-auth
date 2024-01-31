1a8e352c159329c8051dd00c67a10d0258b0078e
michelepiccinno <michele.piccinno@gmail.com>
2024-01-31 17:41:41 +0100

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,27 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\PostController;
+use App\Models\Post;
 
 /* ... */
 
 Route::get('/', function () {
-    return view('welcome');
+    $posts = Post::all();
+    return view('welcome', compact("posts"));
+    // return view('welcome');
 });
 
-require __DIR__ . '/auth.php';
+Route::middleware(['auth'])
+    ->prefix('admin') //definisce il prefisso "admin/" per le rotte di questo gruppo
+    ->name('admin.') //definisce il pattern con cui generare i nomi delle rotte cioè "admin.qualcosa"
+    ->group(function () {
 
-Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
+        //Siamo nel gruppo quindi:
+        // - il percorso "/" diventa "admin/"
+        // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-
-    // Admin Post CRUD
-    Route::resource('posts', PostController::class);
-});
+        Route::resource("posts", PostController::class);
+    });
 
 require __DIR__ . '/auth.php';
